a71865175f5203d7911a14b4cafea371338f5ae4
Agrim Bansal <agrimx2@gmail.com>
2021-02-09 13:43:39 +0530

    Improved readability

--- Frontend/addTask.js
+++ Frontend/addTask.js
@@ -1,7 +1,17 @@
 b = document.getElementById('btn');
 task = document.getElementById('task')
 date = document.getElementById('date')
+currentDate = new Date();
 
+if (currentDate.getDate().toString.length == 2){
+    day = currentDate.getDate()
+}else {
+    day = '0' + currentDate.getDate()  
+}
+
+month = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+date.min = `${currentDate.getFullYear()}-${month[currentDate.getMonth()]}-${day}`
+console.log(date.min)
 
 data = {
     headers : {
@@ -13,9 +23,7 @@ data = {
 
 sendData = async () => {
     
-    if (task.value == "" || date.value == ''){
-        return alert('Enter Valid values for task and date ')
-    } 
+     
 
     dict = {
         task : task.value,
@@ -30,13 +38,32 @@ sendData = async () => {
 
 }
 
+date.addEventListener('keydown', (event) => {
+    if (event.isComposing || event.keyCode === 229){
+       b.click()
+    }
+})
 
 b.addEventListener('click', async () => {
 
         try{
+            if (task.value == "" || date.value == ''){
+                return window.alert('Enter Valid values for task and date ')
+            }
+
+            d = date.value.split('-')
+
+            if(d[0] < currentDate.getFullYear() || d[1] < currentDate.getMonth()+1){
+                return alert(`Please enter date later than today `)
+            }else if (d[1] == currentDate.getMonth()+1 && d[2] < currentDate.getDate()+1){
+                return alert(`Please enter date later than today `)
+            }
+
             b.style.disabled = true;
             await sendData();
+            
             alert('Task Added Successfully')
+            
             b.style.disabled = false;
             task.value = '';
             date.value = '';
